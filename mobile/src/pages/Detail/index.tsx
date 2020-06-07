ecf49363ba2925d42eb3c4b7d26ee6ae408c287c
danyAmaral <danyamaral27@gmail.com>
2020-06-06 23:06:54 -0300

    iniciando aplicação mobile

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
+
+const Detail = () => {
+  
+    return (
+        <View></View>
+    );
+}
+
+export default Detail;
+
